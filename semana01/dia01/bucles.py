71beb29988d4f4f5eedf422a6ae3617c9a575251
devluis7 <dev-luis@outlook.com>
2025-05-31 02:42:26 -0500

    iniciando con python

---
+++ semana01/dia01/bucles.py
@@ -0,0 +1,17 @@
+#while (condicion)
+opcion = 0
+opcion = input("ingrese opcion")
+while (opcion == "0"):
+    print("=========================== Opciones ===========================")
+    print(" 1) Opción 01")
+    print(" 2) Opción 02")
+    print(" 3) Opción 03")
+    print(" 4) Salir")
+    
+    opcion = int(input("Opcion elegida: "))
+    print ("Ud. seleccionó la opción: " + str(opcion))
+    salir = input ("Desea salir (Si/No)")
+    if (salir == "no"):
+        opcion = 0
+    
+          
